faa3bf39a358ad9f116a877c5d5905172f01b39e
tim-green15 <tim.green@ed.ac.uk>
2023-05-15 11:57:55 +0100

    Let's try this again

--- example_postprocessing/C_diagram_png_create.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-Rscript ~/RDM/CARDAMOM/example_postprocessing/Create_latex_C_budget_script_v2_TG.r
-pdflatex ./latex_C_budget_TGtest.tex 
-pdftoppm -png latex_C_budget_TGtest.pdf latex_C_budget_TGtest
-rm latex_C_budget_TGtest.pdf
