298720906b11997f3926e4ae428ae3f65fe0c8b1
Songyan Zhu <zhusy93@gmail.com>
2023-11-27 09:02:30 +0000

    Update split_roi.py

--- Python_utils/GRASS/split_roi.py
+++ Python_utils/GRASS/split_roi.py
@@ -1,4 +1,7 @@
+import numpy as np
+
 # UK small
+
 
 minlon = -8.5
 maxlon = 2
@@ -8,7 +11,27 @@ maxlat = 59.6
 nrow = 3
 ncol = 3
 
-rois = geobox.split_roi(minlon, maxlon, minlat, maxlat, nrow, ncol)
+def split_roi(minlon, maxlon, minlat, maxlat, nrow, ncol):
+    lons = np.linspace(minlon, maxlon, ncol)
+    lats = np.linspace(minlat, maxlat, nrow)
+    lons, lats = np.meshgrid(lons, lats)
+
+    sub_coords = []
+    for i in range(nrow - 1):
+        for j in range(ncol - 1):
+            rect_lons = [
+                lons[i, j], lons[i, j + 1],
+                lons[i + 1, j + 1], lons[i + 1, j]
+            ]
+            rect_lats = [
+                lats[i, j], lats[i, j + 1],
+                lats[i + 1, j + 1], lats[i + 1, j]
+            ]
+            pairs = [list(pair) for pair in zip(rect_lons, rect_lats)]
+            sub_coords.append(pairs)
+    return sub_coords
+
+rois = split_roi(minlon, maxlon, minlat, maxlat, nrow, ncol)
 rois = [roi[0] + roi[2] for roi in rois]
 print(len(rois))
 print(rois)
