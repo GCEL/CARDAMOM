c4d6182554953046c7bd8be3f092e3c2169b225e
Songyan Zhu <zhusy93@gmail.com>
2023-11-28 13:15:00 +0000

    Delete install_config_snap_headless.sh

--- Python_utils/GRASS/install_config_snap_headless.sh
+++
@@ -1,4 +0,0 @@
-wget http://step.esa.int/downloads/7.0/installers/esa-snap_sentinel_unix_7_0.sh
-printf 'o\n2\n2\n/opt/snap\n2,3,4,5\ny\n/usr/local/bin\ny\n\ny' | bash ./esa-snap_sentinel_unix_7_0.sh
-rm ./esa-snap_sentinel_unix_7_0.sh
-snap -- jdkhome  /usr/lib/jvm/java-1.11.0-openjdk-amd64
